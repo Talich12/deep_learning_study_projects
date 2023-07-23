3ac2e6cb251988a20e18183fbc0f987543640af2
talich12 <denis.tal@mail.ru>
2023-07-24 00:25:57 +1000

    feat: Добавлен файл Jubyter Notebook для каждого задания

--- Numpy home task/task6.py
+++ Numpy home task/task6.py
@@ -4,19 +4,22 @@ def run_length_encoding(arr):
     # вычисляем разницу между последующими элементами
     diff = np.diff(arr)
     print(diff)
+
     # находим индексы, где разница не равна 0
     idx = np.where(diff != 0)[0]
     print(idx)
+
     # прибавляем 1, т.к. основная индексация начинается с 0, и к концу добавляем общую длину массива
     idx = np.r_[idx, arr.size - 1]
     print(idx)
+
     # находим элементы
     elements = arr[idx]
 
     # находим количество повторений, учитывая, что numpy разчитывает индексы от 0
     counts = np.diff(np.r_[-1, idx])
 
-    return elements, counts
+    return (elements, counts)
 
 arr = np.array([1, 2, 2, 3, 3, 1, 1, 5, 5, 2, 3, 3])
 elements, counts = run_length_encoding(arr)
